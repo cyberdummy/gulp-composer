18205e772c1f5c6a86e8b96d66b74cd5f9e2b00e
Tom Westcott <tom.westcott@gmail.com>
2014-07-05 00:47:28 +0100

    Initial setup

---
+++ README.md
@@ -0,0 +1,26 @@
+# gulp-composer
+> Install composer files.
+
+## Install
+
+## Examples
+
+```js
+composer = require('gulp-composer');
+
+gulp.task('composer', function () {¬
+	composer({ cwd: './php-stuff', bin: 'composer' });
+});
+```
+
+### Options
+#### options.cwd
+Type: `String`
+
+This is the working directory, normally where the composer.json is located (default to current).
+
+#### options.bin
+Type: `String`
+Default value: `composer`
+
+This is where the composer binary is located. Also possible to add things like "php /somewhere/composer".
---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict'
+var gutil 	= require('gulp-util');
+var shelljs	= require('shelljs/global');
+var through = require('through2');
+
+module.exports = function (opts) {
+
+	var stream = through.obj(function(file, enc, callback) {
+		this.push(file);
+		callback();
+	});
+
+	opts = opts || {};
+
+	opts.cwd = opts.cwd || process.cwd();
+	opts.bin = opts.bin || 'composer';
+	var dir = opts.directory;
+	gutil.log("Using cwd: ", opts.cwd);
+	var commandToRun = opts.bin+" -d="+opts.cwd+" install";
+	gutil.log("Running Composer Command: ", commandToRun);
+	exec(commandToRun, {silent:true}, function(code, output) {
+		if (code != 0)
+			stream.emit('error', new gutil.PluginError('gulp-composer', output));
+		else
+			gutil.log("Composer Completed");
+
+		stream.end();
+		stream.emit("end");
+	});
+
+	return stream;
+};
